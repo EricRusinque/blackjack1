66636b2d156e7318378131e0781169b6fe1d4741
unknown <emprenderusinque@hotmail.com>
2023-01-12 11:11:10 -0500

    valor de cada carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -26,7 +26,6 @@ const crearDeck = () => {
 
     // console.log(deck);
     deck = _.shuffle(deck)
-    console.log(deck)
 }
 
 crearDeck();
@@ -40,10 +39,29 @@ const perdirCarta = () => {
     }
     const carta = deck.pop();
 
-    console.log(deck )
     console.log(carta) // carta debe de ser de la baraja
-    console.log(deck)
 
     return carta
 }
-perdirCarta();
+// perdirCarta();
+
+const valorCarta = ( carta ) => {
+  
+    const valor = carta.substring(0, carta.length - 1);
+    return ( isNaN( valor ) ) ? 
+        ( valor === 'A') ? 11 : 10
+        : valor * 1
+    
+    // let puntos = 0
+
+    // if( isNaN( valor )) {
+    //     puntos = ( valor  === 'A' ) ? 11 : 10;
+    // } else {
+    //     console.log('Es un numero')
+    //     puntos = valor * 1;
+    // }
+    // console.log(puntos)
+}
+
+const valor = valorCarta(perdirCarta());
+console.log({valor})
