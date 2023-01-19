52c8ad8236a5e1ff8caeaba257f1f14f5904591c
unknown <emprenderusinque@hotmail.com>
2023-01-19 18:34:12 -0500

    Optimizacion segunda parte

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -16,8 +16,7 @@
           btnDetener = document.querySelector('#btnDetener'),
           btnNuevo = document.querySelector('#btnNuevo');
 
-    const divCartasJugador =  document.querySelector('#jugador-cartas'),
-          divCartasComputadora =  document.querySelector('#computadora-cartas'),
+    const divCartasJugadores = document.querySelectorAll('.divCartas'),
           puntosHTML = document.querySelectorAll('small');
 
     // esta funcion inicializa el juego
@@ -81,29 +80,38 @@
         // console.log(puntos)
     }
 
-    //
-    const acumularPuntos = () => {
+    // Turno 0 = primer jugador y el utlimo sera la computadora
+    const acumularPuntos = ( carta, turno ) => {
+
+        puntosJugadores[turno] = puntosJugadores[turno] + valorCarta( carta );
+        puntosHTML[turno].innerText = puntosJugadores[turno];
+
+        return puntosJugadores[turno]
 
 
+    }
+
+    const crearCarta = ( carta, turno ) => {
+
+        const imgCarta = document.createElement('img');
+        imgCarta.src = `assets/cartas/${ carta }.png`;
+        imgCarta.classList.add('carta');
+        divCartasJugadores[turno].append( imgCarta )
 
     }
 
     // turno de la computadora 
 
     const turnoComputadora = ( puntosMinimos ) => {
+        let puntosComputadora = 0
 
         do {
             const carta= perdirCarta();
-
-            puntosComputadora = puntosComputadora + valorCarta( carta );
-            puntosHTML[1].innerText = puntosComputadora;
+            puntosComputadora = acumularPuntos(carta, puntosJugadores.length - 1);
+            
+           
+            crearCarta( carta, puntosJugadores.length - 1);
         
-            // <img class="carta" src="assets/cartas/10C.png" alt="">
-            const imgCarta = document.createElement('img');
-            imgCarta.src = `assets/cartas/${ carta }.png`;
-            imgCarta.classList.add('carta');
-        
-            divCartasComputadora.append(imgCarta);
 
             if( puntosMinimos > 21 ) {
                 break;
@@ -112,17 +120,15 @@
         } while ( (puntosComputadora < puntosMinimos) && ( puntosMinimos <= 21) );
 
         setTimeout(() => {
-
-        
-        if( puntosComputadora === puntosMinimos ) {
-            alert('Nadie gana :(')
-        } else if ( puntosMinimos > 21 ) {
-            alert('Computadora gana')
-        }else if ( puntosComputadora > 21 ) {
-            alert('Jugador Gana')
-        } else {
-            alert('Computadora Gana')
-        }
+            if( puntosComputadora === puntosMinimos ) {
+                alert('Nadie gana :(')
+            } else if ( puntosMinimos > 21 ) {
+                alert('Computadora gana')
+            }else if ( puntosComputadora > 21 ) {
+                alert('Jugador Gana')
+            } else {
+                alert('Computadora Gana')
+            }
         }, 10 )
     }
 
@@ -132,17 +138,10 @@
     btnPedir.addEventListener('click', function() {
 
         const carta= perdirCarta();
+        const puntosJugador = acumularPuntos(carta, 0);
 
-        puntosJugador = puntosJugador + valorCarta( carta );
-        puntosHTML[0].innerText = puntosJugador;
-
-        // <img class="carta" src="assets/cartas/10C.png" alt="">
-        const imgCarta = document.createElement('img');
-        imgCarta.src = `assets/cartas/${ carta }.png`;
-        imgCarta.classList.add('carta');
-
-        divCartasJugador.append(imgCarta);
-        
+        crearCarta( carta, 0);
+              
         if( puntosJugador > 21 ) {
             console.warn('Lo siento muchom, perdiste');
             btnPedir.disabled = true;
@@ -175,13 +174,13 @@
         btnPedir.disabled = false;
         btnDetener.disabled = false;
 
-        puntosJugador = 0
-        puntosComputadora = 0
-        puntosHTML[0].innerText = puntosJugador;
-        puntosHTML[1].innerText = puntosJugador;
+        // puntosJugador = 0
+        // puntosComputadora = 0
+        // puntosHTML[0].innerText = 0;
+        // puntosHTML[1].innerText = 0;
 
-        divCartasComputadora.innerHTML = null
-        divCartasJugador.innerHTML = null
+        // divCartasComputadora.innerHTML = null
+        // divCartasJugador.innerHTML = null
         
         crearDeck();
     })
